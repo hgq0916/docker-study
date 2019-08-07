126f1d5cf4d2ad6c19e78d70e27e69664f105145
hugangquan <hugangquan@thizgroup.com>
2019-08-07 16:22:17 +0800

    first commit

---
+++ src/main/java/com/thizgroup/docker/DockerApplication.java
@@ -0,0 +1,27 @@
+package com.thizgroup.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gangquan.hu
+ * @Package: com.thizgroup.docker.DockerApplication
+ * @Description: TODO
+ * @date 2019/8/5 18:37
+ */
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DockerApplication.class,args);
+  }
+
+  @RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
+}
