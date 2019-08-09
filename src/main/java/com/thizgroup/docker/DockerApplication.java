b4a2ea8b9d47fcaa515600697927816840902fe7
hgq0916 <1286908118@qq.com>
2019-08-09 17:58:18 +0800

    docker自动化部署

--- src/main/java/com/thizgroup/docker/DockerApplication.java
+++ src/main/java/com/thizgroup/docker/DockerApplication.java
@@ -1,6 +1,11 @@
 package com.thizgroup.docker;
 
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.SpringApplication;
@@ -31,7 +36,14 @@ public class DockerApplication {
   @RequestMapping("/testGithubWebhooks")
   public String testGithubWebhooks(HttpServletRequest request, HttpServletResponse response){
     System.out.println("testGithubWebhooks be called");
-    System.out.println("request param:"+request.getParameterMap());
+    System.out.println("request param:");
+    Map<String, String[]> parameterMap = request.getParameterMap();
+    Set<Entry<String, String[]>> entries = parameterMap.entrySet();
+    Iterator<Entry<String, String[]>> iterator = entries.iterator();
+    while (iterator.hasNext()){
+      Entry<String, String[]> next = iterator.next();
+      System.out.println(next.getKey()+"="+ Arrays.toString(next.getValue()));
+    }
     System.out.println("request header:"+request.getHeaderNames());
     Enumeration<String> headerNames = request.getHeaderNames();
     while (headerNames.hasMoreElements()){
